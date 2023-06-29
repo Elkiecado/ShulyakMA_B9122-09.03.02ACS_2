01d4693e95857ba9a713bf7d2e15230d261a73fa
Elkiecado <Elkiecado@gmail.com>
2023-06-30 02:56:30 +1000

    Prod

--- ShulyakMA_B9122-09.03.02ACS_2.cpp
+++ ShulyakMA_B9122-09.03.02ACS_2.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <omp.h>
 #include <ppl.h>
+
 int main()
 {
 	srand(time(0));
@@ -16,9 +17,9 @@ int main()
 	}
 	float s1 = 0;
 	double s2 = 0;
-	int threads = 1;
+	int threads = 32;
 	omp_set_num_threads(threads);
-	std::cout << "Количество потоков: " << threads << "\n";
+	std::cout << "Количество используемых потоков: " << threads << "\n";
 	unsigned int t1, t2, t3, t4;
 	t1 = clock();
 #pragma omp parallel
@@ -28,7 +29,7 @@ int main()
 			s1 += Asgl[i];
 	}
 	t2 = clock();
-	std::cout << "Время суммирования массива float: " << t2 - t1 << "\n";
+	std::cout << "Время затраченное на суммирование float: " << t2 - t1 << "\n";
 	t3 = clock();
 #pragma omp parallel
 	{
@@ -37,5 +38,55 @@ int main()
 			s2 += Asgl[i];
 	}
 	t4 = clock();
-	std::cout << "Время суммирования массива double: " << t4 - t3 << "\n";
+	std::cout << "Время затраченное на суммирование double: " << t4 - t3 << "\n";
+	const int n2 = 2;
+	float** Bsgl = new float* [n];
+	for (int i = 0; i < n; ++i)
+		Bsgl[i] = new float[n2];
+	float** Csgl = new float* [10];
+	for (int i = 0; i < 10; ++i)
+		Csgl[i] = new float[n2];
+	double** Bdbl = new double* [n];
+	for (int i = 0; i < n; ++i)
+		Bdbl[i] = new double[n2];
+	double** Cdbl = new double* [10];
+	for (int i = 0; i < 10; ++i)
+		Cdbl[i] = new double[n2];
+	for (int i = 0; i < n; ++i)
+		for (int j = 0; j < n2; ++j)
+		{
+			Bsgl[i][j] = rand();
+			Bdbl[i][j] = rand();
+		}
+
+	s1 = 0;
+	t1 = clock();
+#pragma omp parallel
+	{
+#pragma omp for reduction(+:s1)
+		for (int i = 0; i < n2; ++i) {
+			for (int j = 0; j < n; ++j) {
+				s1 += Asgl[j] * Bsgl[j][i]; std::cout << "";
+			}
+			Csgl[0][i] = s1;
+			s1 = 0;
+		}
+	}
+	t2 = clock();
+	std::cout << "Время затраченное на произведение float: " << t2 - t1 << "\n\n";
+	s2 = 0;
+	t3 = clock();
+#pragma omp parallel
+	{
+#pragma omp for reduction(+:s2)
+		for (int i = 0; i < n2; ++i) {
+			for (int j = 0; j < n; ++j) {
+				s2 += Adbl[j] * Bdbl[j][i]; std::cout << "";
+			}
+			Cdbl[0][i] = s2;
+			s2 = 0;
+		}
+	}
+	t4 = clock();
+	std::cout << "Время затраченное на произведение double: " << t4 - t3 << "\n\n";
 }
